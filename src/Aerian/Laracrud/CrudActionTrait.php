87e75fdd739d6e02e8d661c739ea1e64dc8571a3
Simon Neaves <simon@aerian.com>
2017-01-22 09:11:12 +0000

    Add CrudActionTrait - currently coupled to Aerian\Database\Eloquent\Model

---
+++ src/Aerian/Laracrud/CrudActionTrait.php
@@ -0,0 +1,88 @@
+<?php
+namespace Aerian\Laracrud;
+
+/**
+ * Trait CrudActionTrait
+ * This trait provides functionality for CRUD controllers including implementing default actions
+ */
+trait CrudActionTrait
+{
+    /**
+     * @var
+     * @todo currently assumes an Aerian\Database\Eloquent\Model, but should be decoupled?
+     */
+    protected $_model;
+
+    /**
+     *
+     * @var string
+     */
+    protected $_modelPath = 'App\\';
+
+    public function index($entity, $limit = 5, $offset =0)
+    {
+        $this->setModel($entity);
+        return $this->_getListArray($limit, $offset);
+    }
+
+    public function blueprint($entity, $id = null)
+    {
+        $this->setModel($entity);
+
+        return $this->_getValidRowOr404($id)
+            ->blueprint()
+            ->toNormalizedArray();
+    }
+
+    /**
+     * @param $entityType string e.g. 'product' or 'productCategory'
+     * @return $this
+     */
+    protected function setModel($entityType)
+    {
+        $entityClass = $this->getEntityClass($entityType);
+        $this->_model = new $entityClass;
+        return $this;
+    }
+
+    protected function getEntityClass($entityType)
+    {
+        return $this->_modelPath . ucfirst($entityType);
+    }
+
+    protected function _getListArray($limit, $offset)
+    {
+        $selectColumns = $this->_model->getListColumns();
+        $key = $this->_model->getKeyName();
+        array_unshift($selectColumns, $key);
+
+        $rows = $this->_model->limit($limit)->offset($offset)->get($selectColumns)->keyBy($key);
+
+        return [
+            'columns' => $this->_model->getListColumns(),
+            'itemIds' => $rows->pluck($key),
+            'items' => $rows
+        ];
+    }
+
+    /**
+     * @param $id
+     * @return \Aerian\Database\Eloquent\Model
+     * @todo this is currenctly coupled to Aerian\Database\Eloquent\Model, but should support other types of models
+     */
+    protected function _getValidRowOr404($id)
+    {
+        if ($id) {
+            $row = $this->_model->find($id);
+        } else {
+            $row = $this->_model;
+        }
+
+        if (!$row instanceof \Aerian\Database\Eloquent\Model) {
+            abort(404);
+        }
+
+        return $row;
+    }
+
+}
